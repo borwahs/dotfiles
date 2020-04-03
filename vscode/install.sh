dccda0c0ec55f5523aedcbfa81e6dca77d9469d6
Rob Shaw <rob@borwahs.com>
2020-04-02 22:39:36 -0400

    switch atom => vscode

---
+++ vscode/install.sh
@@ -0,0 +1,3 @@
+ln -s ~/.dotfiles/VSCode/settings.json ~/Library/Application\ Support/Code/User/settings.json
+ln -s ~/.dotfiles/VSCode/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
+ln -s ~/.dotfiles/VSCode/snippets/ ~/Library/Application\ Support/Code/User
