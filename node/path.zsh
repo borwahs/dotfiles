0677d0706cb2da6e4875ac185f7e4d6c912b2f6d
Rob Shaw <rob@borwahs.com>
2020-04-02 23:23:26 -0400

    load NVM under node/

---
+++ node/path.zsh
@@ -0,0 +1,8 @@
+export NVM_DIR="$HOME/.nvm"
+
+# load nvm
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+# configure nvm completion
+# supports zsh - https://github.com/nvm-sh/nvm/blob/master/bash_completion#L83
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
