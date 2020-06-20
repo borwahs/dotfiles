5ecd360f7d30166fd05183ae11278ebec7a11f07
Rob Shaw <rob@borwahs.com>
2020-06-20 15:47:54 -0400

    use link_file for vscode configuration files

--- vscode/install.sh
+++ vscode/install.sh
@@ -1,3 +1,7 @@
-ln -s ~/.dotfiles/VSCode/settings.json ~/Library/Application\ Support/Code/User/settings.json
-ln -s ~/.dotfiles/VSCode/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
-ln -s ~/.dotfiles/VSCode/snippets/ ~/Library/Application\ Support/Code/User
+source "$(dirname $0)/../utils/functions.sh"
+
+info "linking VSCode configuration"
+
+link_file ~/.dotfiles/VSCode/settings.json ~/Library/Application\ Support/Code/User/settings.json
+link_file ~/.dotfiles/VSCode/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
+link_file ~/.dotfiles/VSCode/snippets/ ~/Library/Application\ Support/Code/User/snippets
