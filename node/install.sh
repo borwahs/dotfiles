de0e5e21fcaf9798f645a403122a1a7962ce1840
Rob Shaw <rob@borwahs.com>
2020-06-20 17:07:32 -0400

    move npm global dependency installation under node/install.sh

--- node/install.sh
+++ node/install.sh
@@ -1,4 +1,21 @@
-if test ! $(which spoof)
-then
-  sudo npm install spoof -g
-fi
+source "$(dirname $0)/../utils/functions.sh"
+
+install_npm_dependencies () {
+  if command_exists "npm";
+  then
+    info '› installing npm dependencies'
+
+    # TODO: future hide output and only show if error
+    while IFS= read -r line
+    do
+      npm install $line --global --silent
+    done < "$(dirname $0)/npm-packages.txt"
+
+    success '› installed npm dependencies'
+  else
+    fail '✗ error installing npm dependencies'
+  fi
+}
+
+# run install npm dependencies (global)
+install_npm_dependencies
